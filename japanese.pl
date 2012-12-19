565349df2abbe1f2b23b1fa22c5c2517b044eb4a
Shawn M Moore <code@sartak.org>
2012-12-19 17:59:41 -0500

    真面目、自作

--- japanese.pl
+++ japanese.pl
@@ -16289,3 +16289,14 @@ word(
     furigana   => 'せつだん',
 );
 
+word(
+    date       => '2012-12-19',
+    word       => '真面目',
+    furigana   => 'しんめんもく',
+);
+
+word(
+    date       => '2012-12-19',
+    word       => '自作',
+    furigana   => 'じさく',
+);
